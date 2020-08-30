38027de58d0e174711dd26f29c68934bd21f5aed
Bennett Bierman <bennett.bierman@duke.edu>
2020-08-30 00:22:19 -0400

    updated unit tests

--- inventory-allocator/test_InventoryAllocator.py
+++ inventory-allocator/test_InventoryAllocator.py
@@ -72,7 +72,7 @@ class TestInventoryAllocator(unittest.TestCase):
         result = InventoryAllocator(order, warehouses).complete_order()
         self.assertEqual(expected_value, result)
 
-    # tests when warehouses split a two item order but both have items not in the order
+    # tests when warehouses split a two item order but neither have every item in the order
     def test_nine(self):
         order = {'apple':15, 'banana': 15, 'cherry': 15}
         warehouses = [Warehouse('w1', {'apple':15, 'banana':7}), Warehouse('w2', {'banana':8, 'cherry':15})]
@@ -80,8 +80,24 @@ class TestInventoryAllocator(unittest.TestCase):
         result = InventoryAllocator(order, warehouses).complete_order()
         self.assertEqual(expected_value, result)
 
-    # tests when warehouses split a two item order but both have items not in the order and not all the inventory is needed
+    # tests when a Warehouse does not have an item from the order
     def test_ten(self):
+        order = {'apple': 15, 'banana': 15}
+        warehouses = [Warehouse('w1', {'apple': 15, 'cherry': 15})]
+        expected_value = []
+        result = InventoryAllocator(order, warehouses).complete_order()
+        self.assertEqual(expected_value, result)
+
+    # tests when a Warehouse has items not in the order
+    def test_eleven(self):
+        order = {'apple': 15, 'cherry': 15}
+        warehouses = [Warehouse('w1', {'apple': 15, 'banana': 15, 'cherry': 15})]
+        expected_value = [Warehouse('w1', {'apple': 15, 'cherry': 15})]
+        result = InventoryAllocator(order, warehouses).complete_order()
+        self.assertEqual(expected_value, result)
+
+    # tests when warehouses split a two item order but both have items not in the order and not all the inventory is needed
+    def test_twelve(self):
         order = {'apple': 15, 'banana': 15, 'cherry': 15}
         warehouses = [Warehouse('w1', {'apple': 100, 'banana': 14, 'duck':4}), Warehouse('w2', {'banana': 100, 'cherry': 100})]
         expected_value = [Warehouse('w1', {'apple': 15, 'banana': 14}), Warehouse('w2', {'banana': 1, 'cherry': 15})]
@@ -89,7 +105,7 @@ class TestInventoryAllocator(unittest.TestCase):
         self.assertEqual(expected_value, result)
 
     # test when warehouses have items not in order and do not have an item in the order
-    def test_eleven(self):
+    def test_thirteen(self):
         order = {'apple': 15, 'banana': 15, 'cherry': 15}
         warehouses = [Warehouse('w1', {'apple': 100, 'squash': 14, 'duck': 4}), Warehouse('w2', {'banana': 100, 'car': 100})]
         expected_value = []
@@ -97,31 +113,50 @@ class TestInventoryAllocator(unittest.TestCase):
         self.assertEqual(expected_value, result)
 
     # test when warehouses and order are empty sets and maps respectively
-    def test_twelve(self):
+    def test_fourteen(self):
         order = {}
         warehouses = []
         expected_value = []
         result = InventoryAllocator(order, warehouses).complete_order()
         self.assertEqual(expected_value, result)
 
+    # tests when item in order is of quantity 0
+    def test_fifteen(self):
+        order = {'apple': 0}
+        warehouses = [Warehouse('w1', {'apple': 100, 'banana': 19})]
+        expected_value = []
+        result = InventoryAllocator(order, warehouses).complete_order()
+        self.assertEqual(expected_value, result)
+
     # test when order has items of quantity 0
-    def test_thirteen(self):
-        order = {'apple': 0, 'banana': 0, 'cherry': 0}
-        warehouses = [Warehouse('w1', {'apple': 100, 'banana': 14, 'duck': 4}),  Warehouse('w2', {'banana': 100, 'cherry': 100})]
+    def test_sixteen(self):
+        order = {'apple': 10, 'banana': 0}
+        warehouses = [Warehouse('w1', {'apple': 100, 'banana': 19}),  Warehouse('w2', {'banana': 100, 'cherry': 100})]
         expected_value = []
         result = InventoryAllocator(order, warehouses).complete_order()
         self.assertEqual(expected_value, result)
 
     # test when warehouse's map and order map are in different orders
-    def test_fourteen(self):
+    def test_seventeen(self):
         order = {'apple': 100, 'banana': 200, 'cherry': 300}
         warehouses = [Warehouse('w1', {'cherry': 500, 'apple': 200, 'banana': 400})]
         expected_value = [Warehouse('w1', {'apple': 100, 'banana': 200, 'cherry': 300})]
         result = InventoryAllocator(order, warehouses).complete_order()
         self.assertEqual(expected_value, result)
 
+    # tests when not every warehouse needs to ship items
+    def test_eighteen(self):
+        order = {'apple': 100, 'banana': 200, 'cherry': 300}
+        warehouses = [Warehouse('w1', {'apple': 50, 'banana': 50, 'cherry': 50}),
+                      Warehouse('w2', {'apple': 300, 'banana': 300, 'cherry': 300}),
+                      Warehouse('w2', {'apple': 50, 'banana': 50, 'cherry': 50})]
+        expected_value = [Warehouse('w1', {'apple': 50, 'banana': 50, 'cherry': 50}),
+                          Warehouse('w2', {'apple': 50, 'banana': 150, 'cherry': 250})]
+        result = InventoryAllocator(order, warehouses).complete_order()
+        self.assertEqual(expected_value, result)
+
     # tests when there is a large order, many warehouses, extra items in the warehouses, and some extra inventory
-    def test_fifteen(self):
+    def test_nineteen(self):
         order = {'apple': 1000, 'banana': 1000, 'cherry': 1000, 'duck': 1000, 'eggs': 10000}
         warehouses = [Warehouse('w1', {'car': 500, 'apple': 100}),
                       Warehouse('w2', {'banana':1000, 'duck': 500, 'cherry':800}),
